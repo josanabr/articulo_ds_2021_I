5f18a590b7cd7801d505fcd7c167e34e4b87f2f0
alejandro <alejandrobravo0329@gmail.com>
2021-05-07 16:47:24 -0500

    mySql script

--- codigo/Docker/mySql.sh
+++ codigo/Docker/mySql.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo docker pull mysql/mysql-server:8.0
-docker run --name=my-sql-docker -d mysql/mysql-server:8.0
+sudo docker run --name=my-sql-docker -d mysql/mysql-server:8.0
