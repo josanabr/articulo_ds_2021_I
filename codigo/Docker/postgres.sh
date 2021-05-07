4c0a37c2f83e42b7e5fcd63c82e37eb9767c1f4e
alejandro <alejandrobravo0329@gmail.com>
2021-05-07 14:36:16 -0500

    my sql script

--- codigo/Docker/postgres.sh
+++ codigo/Docker/postgres.sh
@@ -2,5 +2,6 @@
 
 echo "install postgres image"
 sudo docker pull postgres:13.2
+echo "start postgres container"
 sudo docker run --name posCont -e POSTGRES_PASSWORD=pwd -p 5432:5432 -d postgres:13.2
-sudo docker exec -it posCont psql -U postgres -d postgres -a -f coffeemach-ddl.sql
+#sudo docker exec -it posCont psql -U postgres -d postgres -a -f coffeemach-ddl.sql
